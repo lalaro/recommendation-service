088b022943a8fb16626d90146994973709459b62
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-10-29 16:58:23 -0500

    Document MobileApp class and its constructor
    Added documentation for MobileApp class and constructor parameters.

--- src/models/MobileApp.js
+++ src/models/MobileApp.js
@@ -1,1 +1,14 @@
-
+/**
+ * Class representing a mobile application.
+ * @param {Express} expressServer - The Express server instance.
+ * @param {number} port - The port number the server listens on.
+ * @param {object} routes - The routes configuration for the application.
+ */
+class MobileApp {
+    constructor(expressServer, port, routes) {
+        this.expressServer = expressServer;
+        this.port = port;
+        this.routes = routes;
+    }
+}
+module.exports = MobileApp;
